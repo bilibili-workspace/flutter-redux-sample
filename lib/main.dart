4045ed8ff70d8066beaa4248d40d5b70398e03c0
圆号本昊 <814484626@qq.com>
2021-03-27 02:28:55 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/app_state.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+
 import 'package:redux/redux.dart';
 
-import 'app_state.dart';
-import 'home_store.dart';
-
 void main() {
-  runApp(FlutterReduxApp());
+  runApp(MyApp());
 }
 
-class FlutterReduxApp extends StatelessWidget {
-  //步骤4 申明一个store,必须传入一个方法进去，其余的参数选填，这里选择初始化一下基础参数
-  final store = Store<AppState>(counterReducer, initialState: AppState.initialState());
+class MyApp extends StatelessWidget {
 
+    final store = Store<AppState>(counterReducer, initialState: AppState.initialState());
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return StoreProvider(
         store: store, //绑定store
         child: MaterialApp(
-          home: Scaffold(
-            appBar: AppBar(
-              title: Text("Title"),
-            ),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text("You have pushed the button this many times:"),
-                  //当需要使用到共享数据的时候使用StoreConnector来获取数据
-                  StoreConnector<AppState, String>(builder: (context, value) {
-                    return Text(value, style: Theme.of(context).textTheme.display1);
-                  }, converter: (Store store) {
-                    return store.state.count.toString();
-                  })
-                ],
-              ),
-            ),
-            floatingActionButton: StoreConnector<AppState, VoidCallback>(
-              converter: (Store store) {
-                return () => store.dispatch(IncrementAction(payload: 1)); //发送数据
-              },
-              builder: (BuildContext context, VoidCallback callback) {
-                return FloatingActionButton(
-                    onPressed: callback, child: Icon(Icons.add));
-              },
-            ),
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
           ),
-        ));
+          home: MyHomePage(title: 'Flutter Demo Home Page'),
+      )
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            StoreConnector<AppState, String>(builder: (context, value) {
+              return Text(
+                '$value',
+                style: Theme.of(context).textTheme.headline4,
+              );
+            }, converter: (Store store) {
+              return store.state.count.toString();
+            }),
+          ],
+        ),
+      ),
+        floatingActionButton: StoreConnector<AppState, VoidCallback>(
+          converter: (Store store) {
+            return () => store.dispatch(IncrementAction(payload: 1)); //发送数据
+          },
+          builder: (BuildContext context, VoidCallback callback) {
+            return FloatingActionButton(
+                onPressed: callback, child: Icon(Icons.add));
+          },
+        ),
+    );
   }
 }
